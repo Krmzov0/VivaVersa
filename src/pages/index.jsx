31f4f9af7e12bd7ee947a80ce9488a00b262a721
Krmzov0 <krmzovgorgi0@gmail.com>
2023-09-29 18:55:43 +0200

    mobile responsiveness

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,10 +12,10 @@ export default function Home() {
   return (
     <div className="">
 
-      <div className=" relative flex justify-start sm:justify-between flex-col items-center h-screen w-full">
+      <div className="relative flex justify-start sm:justify-between flex-col items-center h-max sm:h-screen w-full">
         <Header />
 
-        <div className="flex flex-col justify-center items-center mt-20 sm:mt-0 relative">
+        <div className="flex flex-col justify-center items-center mt-14 sm:mt-0 relative">
           <p className="text-[#878c8e] unbounded font-light mb-3 sm:md-4 text-sm sm:text-[16px] text-center">DIGITAL MARKETING AGENCY</p>
 
           <div>
@@ -24,17 +24,16 @@ export default function Home() {
         </div>
 
 
-        <div className="w-max relative mb-0 mt-16 sm:mt-0 sm:mb-14 flex-col sm:flex-row flex gap-x-12 gap-y-6 sm:gap-y-0 justify-between items-center">
+        <div className="w-max relative mb-0 mt-16 sm:mt-0 sm:mb-14 flex-row sm:flex-row flex gap-x-6 sm:gap-x-12 gap-y-6 sm:gap-y-0 justify-between items-center">
           <div className=" h-max w-max flex flex-col gap-y-8 justify-center items-center">
             <motion.div initial={{ scaleY: 0 }} animate={{ scaleY: 1 }} transition={{ delay: 0.5 }} className="origin-bottom"><div className="w-14 h-14 relative bg-[#17191b] flex justify-center items-center">
               <motion.div initial={{ scaleY: 0 }} animate={{ scaleY: 1 }} transition={{ delay: 0.9 }} className="origin-center w-14 h-4 bg-[#878c8e] absolute"><div className="w-14 h-4 bg-[#878c8e] absolute"></div></motion.div>
               <motion.div initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ delay: 0.7 }} className="origin-center"><div className="w-8 h-8 bg-[#f1f2ee]"></div></motion.div>
             </div></motion.div>
-            <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1 }} ><h3 className="text-lg text-center unbounded font-light uppercase">Social Media <br /> Marketing</h3></motion.div>
+            <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1 }} ><h3 className="text-md sm:text-lg text-center unbounded font-light uppercase">Social Media <br /> Marketing</h3></motion.div>
           </div>
 
-          <div className="h-full w-[1px] bg-[#878c8e47] hidden sm:flex"></div>
-          <div className="h-[1px] w-full bg-[#878c8e47] flex sm:hidden"></div>
+          <div className="h-full w-[1px] bg-[#878c8e47]  flex"></div>
 
           <div className="h-max w-max flex flex-col gap-y-8 justify-center items-center">
             <motion.div initial={{ scaleY: 0 }} animate={{ scaleY: 1 }} transition={{ delay: 1.2 }} className="origin-top w-14 h-14 bg-[#17191b] flex justify-center items-center"><div className="w-14 h-14 bg-[#17191b] flex justify-center items-center">
@@ -43,26 +42,26 @@ export default function Home() {
                 <div className="w-4 h-4 bg-[#17191b]"></div>
               </div></motion.div>
             </div></motion.div>
-            <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.9 }} ><h3 className="text-lg text-center unbounded text-[#17191b] font-light uppercase">Website <br /> Developement</h3></motion.div>
+            <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.9 }} ><h3 className="text-md sm:text-lg text-center unbounded text-[#17191b] font-light uppercase">Website <br /> Developement</h3></motion.div>
           </div>
         </div>
       </div>
 
-      <div className="p-12 w-screen h-full">
-        <div className="mt-16 flex justify-center items-center w-full h-[100vh] bg-[#17191b]">
+      <div className="p-8 sm:p-12 w-screen h-full flex ">
+        <div className="mt-16 flex justify-center items-center w-full h-[30vh] sm:h-[100vh] bg-[#17191b]">
           <Image
-            src="/LogoWhite.png"
-            width={260}
-            height={260}
+            src="/White.png"
+            width={80}
+            height={80}
             alt=''
             priority={true}
           />
         </div>
       </div>
 
-      <div className="h-screen w-screen p-12 hidden sm:flex flex-col justify-start">
-        <div className="w-full justify-between flex items-center">
-          <h1 className="unbounded text-[38px] font-light">GET TO <br />KNOW US BETTER</h1>
+      <div className="h-max sm:h-screen relative w-screen p-8 sm:p-12 pb-16 sm:pb-12 sm:pr-16 flex flex-col justify-start">
+        <div className="w-full justify-between gap-y-8 sm:gap-y-0 flex flex-col-reverse sm:flex-row  items-end sm:items-center">
+          <h1 className="unbounded text-[38px] font-light text-right sm:text-left">GET TO <br />KNOW US BETTER</h1>
 
           <div onClick={() => router.push('/about')} className="flex aboutLink cursor-pointer relative items-center gap-x-4">
             <h2 className="unbounded text-xl text-[#17191b] font-light relative">ABOUT</h2>
@@ -77,22 +76,23 @@ export default function Home() {
           <p className="unbounded text-[16px] font-regular text-[#878c8e] hidden">At VivaVersa, we're more than just a business. We're a team of passionate individuals <br /> dedicated to briefly describe your mission and values</p>
         </div> */}
 
-        <div className="mt-24 relative">
-          <h1 className="text-[34px] uppercase unbounded text-[#17191b] font-light leading-[70px]">Driving digital success through <br /> expert marketing strategies and <br /> data-driven solutions. </h1>
+        <div className="mt-24 relative hidden">
+          <h1 className="text-[34px] uppercase unbounded text-[#17191b] font-light leading-[70px]">Driving digital success through expert marketing strategies and <br /> data-driven solutions. </h1>
         </div>
 
-
+        <p className=" mt-10 unbounded text-[16px] text-[#878c8e]">Driving digital success through expert marketing strategies and <br /> data-driven solutions.</p>
+        
       </div>
 
-      <div className="z-10 w-screen h-full gap-y-20 hidden sm:flex flex-col bg-[#17191b] p-12 pr-0  sm:pr-16">
+      <div className="z-10 w-screen relative h-full  gap-y-16 sm:gap-y-20 flex flex-col bg-[#17191b] p-8 py-16 sm:py-12 sm:p-12  sm:pr-16">
 
         <div className="flex flex-col">
 
 
-          <div className="w-full justify-between flex items-center">
+          <div className="w-full justify-between gap-y-8 sm:gap-y-0 flex flex-col-reverse sm:flex-row  items-end sm:items-center">
 
             <div className="flex flex-col">
-              <h1 className="unbounded text-[38px] font-light text-[#f1f2ee]">SERVICES <br /> WHAT WE’RE GOOD AT</h1>
+              <h1 className="unbounded text-[38px] font-light text-[#f1f2ee] text-right sm:text-left">SERVICES <br /> WHAT WE’RE GOOD AT</h1>
             </div>
 
             <div onClick={() => router.push('/services')} className="flex servicesLink cursor-pointer relative items-center gap-x-4">
@@ -110,15 +110,15 @@ export default function Home() {
 
         <div className="">
           <div className="py-10 border-b-2 border-[#f1f2ee]">
-            <h1 className="text-[32px] text-[#f1f2ee] unbounded font-light gap-x-4 flex items-center"><span className="text-[#878c8e]">01</span> SOCIAL MEDIA MARKETING</h1>
+            <h1 className="text-2xl sm:text-[32px] text-[#f1f2ee] unbounded font-light gap-x-4 flex items-center"><span className="text-[#878c8e]">01</span> SOCIAL MEDIA MARKETING</h1>
           </div>
 
           <div className="py-10 border-b-2 border-[#f1f2ee]">
-            <h1 className="text-[32px] text-[#f1f2ee] unbounded font-light gap-x-4 flex items-center"><span className="text-[#878c8e]">03</span> WEB DESIGN</h1>
+            <h1 className="text-2xl sm:text-[32px] text-[#f1f2ee] unbounded font-light gap-x-4 flex items-center"><span className="text-[#878c8e]">02</span> WEB DESIGN</h1>
           </div>
 
           <div className="py-10 border-b-2 border-[#f1f2ee]">
-            <h1 className="text-[32px] text-[#f1f2ee] unbounded font-light gap-x-4 flex items-center"><span className="text-[#878c8e]">02</span> WEB DEVELOPMENT</h1>
+            <h1 className="text-2xl sm:text-[32px] text-[#f1f2ee] unbounded font-light gap-x-4 flex items-center"><span className="text-[#878c8e]">03</span> WEB DEVELOPMENT</h1>
           </div>
         </div>
       </div>
